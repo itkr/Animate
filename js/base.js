d357fd94b3c5ff5be7870161148fac35f6f2b80b
itkr <g600392@gmail.com>
2012-11-24 02:27:09 +0900

    Change naming agreement
    View名とDOMのIDを一致させる規約にした

--- js/base.js
+++ js/base.js
@@ -42,25 +42,13 @@
 	/**
 	 * Views
 	 * One object one element
-	 * テンプレートメソッドのような設計にする(TODO)
+	 * テンプレートメソッドのような設計に
 	 * このオブジェクト以外でDOM操作を行わない
 	 */
 	ITKR_ANIM.views = (function(document){
-	
-		// @TODO 汎用的に使えるメソッドをベースオブジェクトにまとめてコンクリートオブジェクトで継承する
-		
-		// デバッグ用
-		//var puts = function(str){
-		//	var element = $('console');
-		//	element.innerHTML = str + '<br>' + element.innerHTML;
-		//};
-		
+
 		var objects = {
 			WorldView : function(){
-				var element = this.$('world');
-				this.set = function(innerElement){
-					element.innerHTML = innerElement;
-				};
 			},
 		};
 		return objects;
@@ -71,13 +59,13 @@
 	 * ベースとなるオブジェクト群
 	 */
 	ITKR_ANIM.core = {
-		View : function(document){
-			this.$ = function(id){
-				return document.getElementById(id);
+		View : function(element){
+			this.set = function(innerElement){
+				element.innerHTML = innerElement;
 			};
 		},
 		Model : function(){
-			this.meta = 'NotExtended'
+			this.meta = 'NotExtended';
 		},
 	};
 	
@@ -86,10 +74,12 @@
 	 */
 	ITKR_ANIM.init = function(document){
 		for(key in this.views){
-			this.views[key].prototype = new this.core.View(document);
+			var view = this.views[key];
+			view.prototype = new this.core.View(document.getElementById(key));
 		}
 		for(key in this.models){
-			this.models[key].prototype = new this.core.Model();
+			var model = this.models[key];
+			model.prototype = new this.core.Model();
 		}
 	};
 	
