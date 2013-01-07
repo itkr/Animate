93a2c095cf8993442c5663853553cfccd5848075
itkr <g600392@gmail.com>
2013-01-08 02:06:03 +0900

    Updated actual condition object

--- js/base.js
+++ js/base.js
@@ -341,21 +341,39 @@
 		var models = $A.Models;
 		// プロジェクト
 		var world = $A.fn.init(document);
-		// 最初のシーン
+
+		// ---- 最初のシーン ----
 		var scene1 = world.addScene(function() {
 
+			// box view
+			this.addView(function() {
+				this.createBox();
+			}, 'scene1_view1');
+			
+			// canvas view
+			this.addCanvas(function(width, height) {
+				this.drawFillTriangle(width / 2, height / 2, 60, '#3366ff');
+				this.drawStrokeTriangle(width / 2, height / 2, 60, {
+					color : '#cc6666',
+					lineWidth : 6
+				});
+				this.draw(width, height);
+			}, 'scene1_canvas1');
+
 		}, 'scene1');
-		scene1.addView(function() {
-			this.createBox();
-		}, 'scene1_view1');
-		scene1.addCanvas(function(width, height) {
-			this.drawFillTriangle(width / 2, height / 2, 60, '#3366ff');
-			this.drawStrokeTriangle(width / 2, height / 2, 60, {
-				color : '#cc6666',
-				lineWidth : 6
-			});
-			this.draw(width, height);
-		}, 'scene1_canvas1');
+
+
+		// scene1.addView(function() {
+		// this.createBox();
+		// }, 'scene1_view1');
+		// scene1.addCanvas(function(width, height) {
+			// this.drawFillTriangle(width / 2, height / 2, 60, '#3366ff');
+			// this.drawStrokeTriangle(width / 2, height / 2, 60, {
+				// color : '#cc6666',
+				// lineWidth : 6
+			// });
+			// this.draw(width, height);
+		// }, 'scene1_canvas1');
 	})();
 
 })(window.document);
